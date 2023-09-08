8ec9e6a18fc04e5543f9ba67d063d2c336111de8
Blaise Swartwood <swartwba@rose-hulman.edu>
2023-09-08 13:50:39 -0400

    FollowAlong2

---
+++ Web-app-colorify/scripts/main.js
@@ -0,0 +1,9 @@
+function paint(color) {
+    console.log(color);
+    const circle = document.getElementById('circleID');
+    //circle.style = 'background-color:${color}';
+    circle.style.backgroundColor = color;
+    console.log(circle);
+    console.log('Script Loaded...');
+}
+
