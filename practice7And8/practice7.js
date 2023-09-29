6fcae95f2bd60801df484359bc1b9880bb090120
Blaise Swartwood <swartwba@rose-hulman.edu>
2023-09-29 13:30:09 -0400

    practice7done

---
+++ practice7And8/practice7.js
@@ -0,0 +1,75 @@
+//1
+
+for(let i = 2; i <= 10; i+=2)
+{
+    console.log(i);
+}
+
+console.log();
+
+//2
+
+for(let i = 5; i > 0; i--)
+{
+    console.log(i);
+}
+
+console.log();
+
+//3
+
+const numbers = [1,2,3,4,5];
+let sum = 0;
+for(const num of numbers)
+{
+    sum += num;
+}
+
+console.log(sum);
+
+console.log();
+
+//4
+
+const student = {name: 'John', age: 20, grade: 'A'};
+
+for(const key in student)
+{
+    console.log(`${key}: ${student[key]}`);
+}
+
+console.log();
+
+//5
+
+const book = {title: "Harry Potter", author: "JK Rowling", year: 1997};
+
+for(const info in book)
+{
+    console.log(`${info}: ${book[info]}`);
+}
+
+console.log();
+
+//6
+
+const colors = ["red", "green", "blue", "yellow"];
+for(const color of colors)
+{
+    console.log(color);
+}
+
+console.log();
+
+//7
+
+//numbers already assigned and still the same as above
+sum = 0;
+for(const num of numbers)
+{
+    sum += num;
+}
+
+console.log(sum);
+
+console.log();
