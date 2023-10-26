77360b8fd2e7d071e6aa6ee517a050d9d3b3ade3
Blaise Swartwood <swartwba@rose-hulman.edu>
2023-10-26 18:28:26 -0400

    FA10

---
+++ Postman/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+const port = 3000
+app.listen(port, () =>{
+    console.log("Server Listenening on PORT:", port);
+});
+app.get("/status", (request, response) => {
+    const status = {
+        "Status": "Running"
+    };
+    response.send(status)
+});
